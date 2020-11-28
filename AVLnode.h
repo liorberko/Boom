57599ade435fbc0bfb5b81bb14f66c736d4cc1b3
uvalvered <63462153+uvalvered@users.noreply.github.com>
2020-11-28 14:10:18 +0200

    AVLtree push

--- AVLnode.h
+++ AVLnode.h
@@ -9,8 +9,9 @@ class node
     int balance;
     node* left_son;
     node* right_son;
-    node* prev; 
-    AVLnode(T info, node* left_son = nullptr, node* right_son = nullptr, node* prev = nullptr) : info(info), left_son(left_son)
-}
+    node* parent; 
+    AVLnode(T info, node* left_son = nullptr, node* right_son = nullptr, node* prev = nullptr) : info(info), left_son(left_son),right_son(right_son),parent(parent);
+    ~AVLnode() = default;
+};
 
 #endif /* NODE */
--- avlTree.h
+++ avlTree.h
@@ -1,9 +1,42 @@
 #ifndef AVLTREE
 #define AVLTREE
 #include "AVLnode.h"
+#include "library.h"
 
+template <class T>
+class AVLtree
+{
+    node<T> *root;
+    node<T> *bigest;
+    node<T> *smallest;
+    StatusType rotateRight(const node<T> *vertex);
+    StatusType rotateLeft(const node<T> *vertex);
+    StatusType rotateLeftToRight(const node<T> *vertex);
+    StatusType rotateRightThenLeft(const node<T> *vertex);
 
+    public:
+    AVLtree();
+    ~AVLtree();
+    StatusType addVertex(const node<T> *new_vertex);
+    StatusType removeVertex(int key);
+};
+/**************************************/
+/*     C'tors and D'tors section      */
+/**************************************/
+template <class T>
+AVLtree(root=NULL,bigget=NULL,smallest=NULL):root(root),biggest(biggest),smallest(smallest);
 
+template <class T>
+~AVLtree()
+{
+    node<t> *current;
+    while (current.rightSun 
+    {
+        /* code */
+    }
+}
+template <class T>
+StatusType addVertex(const node<T> *new_vertex)  
 
 
 
